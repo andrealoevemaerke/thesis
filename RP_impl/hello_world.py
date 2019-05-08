424feaaafb1ffa4850b9b4643967e201fe982491
andrealoevemaerke <andreatram@gmail.com>
2019-05-08 11:11:41 +0200

    ok

--- RP_impl/hello_world.py
+++ RP_impl/hello_world.py
@@ -60,9 +60,9 @@ class commsThread (Thread):
       
 m = 7979490791
 F = field.GF(m)            
-n = 4
+n = 2
 t = 1
-x = 5
+x = 7
 
 ipv4 = os.popen('ip addr show eth0').read().split("inet ")[1].split("/")[0]
 pnr = party_addr.index([ipv4, port])
@@ -73,16 +73,114 @@ q3 = que.Queue()
 #Initialization..
 #TCP_IP = '192.168.100.246'
 #TCP_PORT = 62
-UDP_PORT2 = 3000
+#UDP_PORT2 = 3000
 server_info = party_addr[pnr]#(TCP_IP, TCP_PORT)
-server2_info = (ipv4, UDP_PORT2)
+#server2_info = (ipv4, UDP_PORT2)
 
 # Create new threads..
 t1_comms = commsThread(1, "Communication Thread", server_info,q)
 #2_commsSimulink = UDPcommsThread(2, "t2_commsSimulink", server2_info)
 #ploting = plotter(q3)
 #ploting.start()
-#p = party(F,int(x),n,t,pnr, q, q2, q3, party_addr, server_addr)
+
+
+class party(Thread):
+    def __init__(self, F, x, n, t, i, q, q2,q3, paddr, saddr):
+        Thread.__init__(self)
+        self.c = 0
+        self.comr = 0
+        self.recv = {}
+        self.F = F
+        self.x = x
+        self.n = n
+        self.t = t
+        self.i = i
+        self.q = q
+        self.q2 = q2
+        self.q3 = q3
+        self.party_addr = paddr
+        self.server_addr = saddr
+        
+    def distribute_shares(self, sec):
+        shares = ss.share(self.F, sec, self.t, self.n)
+        for i in range(self.n):
+            sock.TCPclient(self.party_addr[i][0], self.party_addr[i][1], ['input' + str(self.i) , int(str(shares[i]))])
+        
+    def broadcast(self, name, s):
+        for i in range(self.n):
+            sock.TCPclient(self.party_addr[i][0], self.party_addr[i][1], [name + str(self.i) , int(str(s))])
+                    
+    def readQueue(self):
+        while not self.q.empty():
+            b = self.q.get()[1]
+            self.recv[b[0]] = b[1]
+            self.q3.put([b[0][-1], b[1]])
+    
+    def get_shares(self, name):
+        res = []
+        for i in range(self.n):
+            while name + str(i) not in self.recv:
+                self.readQueue()    
+            res.append(self.F(self.recv[name+str(i)]))
+            del self.recv[name + str(i)]
+        return res
+            
+    def reconstruct_secret(self, name):
+        return ss.rec(self.F, self.get_shares(name))
+    
+    def get_share(self, name):
+        while name not in self.recv:
+            self.readQueue()
+        a = self.F(self.recv[name])
+        del self.recv[name]
+        return a
+    def get_triplets(self):
+        while 'triplets' not in self.recv:
+            self.readQueue()
+        b = self.recv['triplets']
+        res = []
+        for i in b:
+            res.append([self.F(j) for j in i])
+        self.triplets = res
+    
+    def mult_shares(self, a, b):
+        r = self.triplets[self.c]
+        self.c += 1
+        
+        d_local = a - r[0]
+        self.broadcast('d' + str(self.comr), d_local)
+        d_pub = self.reconstruct_secret('d' + str(self.comr))
+        self.comr +=1
+        
+        e_local = b - r[1]
+        self.broadcast('e' + str(self.comr), e_local)
+        e_pub = self.reconstruct_secret('e' + str(self.comr))
+        self.comr+=1
+        
+        return d_pub * e_pub + d_pub*r[1] + e_pub*r[0] + r[2]
+    
+    def legendreComp(self,a,b):
+        r = self.triplets[self.c]
+        self.c+=1
+        t = self.tt
+        g = a - b
+        k = self.mult_shares(t, self.mult_shares(r[0], r[0]))
+        j_loc = self.mult_shares(g, k)
+        self.broadcast('j'+ str(self.comr), j_loc)
+        j_pub = self.reconstruct_secret('j'+str(self.comr))
+        self.comr+=1
+        
+        ex = (self.F.p-1)/2
+        sym = pow(int(str(j_pub)),int(ex), self.F.p)
+        f = sym * t
+        c = self.mult_shares((f+1), self.F(2).inverse())
+        return c
+        
+    #def run(self):
+     #   self.distribute_shares(x,'x_shares')  
+              
+        
+p = party(F,int(x),n,t,pnr, q, q2, q3, party_addr, server_addr)
 
 # Start new Threads
 #t2_commsSimulink.start()
@@ -97,10 +195,10 @@ while True:
       continue
 
 print('Connected to 5!')
-#p.start()
+p.start()
 
 
-sock.TCPclient(party_addr[5][0], party_addr[5][1], ['output', int(str(64))])
+sock.TCPclient(party_addr[5][0], party_addr[5][1], ['output', int(p.x)])
 
 while True:
       if not q.empty():
