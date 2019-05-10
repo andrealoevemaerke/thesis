ad6722769d53dec960ea60e7484d49e54469d425
andrealoevemaerke <andreatram@gmail.com>
2019-05-10 14:05:06 +0200

    update

--- RP_impl/car.py
+++ RP_impl/car.py
@@ -118,14 +118,14 @@ print('connection ok')
 a1_share=ss.share(F, x, t, n)
 x= y
 a2_share= ss.share(F, x, t, n)
-
+print('constructed shares in car')
 # send shares to clouds :
 for i in range(n):
     sock.TCPclient(party_addr[i+1][0], party_addr[i+1][1], ['a1'+str(pnr) , int(str(a1_share[i]))])
     sock.TCPclient(party_addr[i+1][0], party_addr[i+1][1], ['a2'+str(pnr) , int(str(a1_share[i]))])
    
     # dublere linje for alle shares eller skal der tjekkes for forbindelse hver gang?  
-
+print('car has send shares')
 
 
 # computations in clouds here for a long time
