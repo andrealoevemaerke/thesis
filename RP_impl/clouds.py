510a7542669e62458634b754b1c95acb57ccf11c
andrealoevemaerke <andreatram@gmail.com>
2019-05-11 16:27:19 +0200

    ok

--- RP_impl/clouds.py
+++ RP_impl/clouds.py
@@ -306,7 +306,6 @@ class party(Thread):
         e11=np.array(np.zeros((nn,l)))
 
         C_shares=np.array(([e00[0,0], e00[0,1], e01[0,0]],[e00[1,0], e00[1,1], e01[1,0]], [e10[0,0], e10[0,1], 0],[ e10[1,0], e10[1,1], 0]))
-
         #print('C matrix:', C_shares)
           
         f = []
@@ -314,49 +313,47 @@ class party(Thread):
         r = []
         
         for k in range(0, mu):
-                
-          broad_ckk= self.mult_shares(share_ran,C_shares[k,k])
-          self.broadcast('c_kk' + str(self.comr), broad_ckk)
-          
-          # protocol line 5 
-          r_temp.append(self.reconstruct_secret('c_kk'+str(self.comr))) 
-          #print('r is:', r_temp)
-          print('1print')
-          if r_temp[k] == 0:
-             r.append(0)
-          elif r_temp[k] != 0:
-             r.append(1)
-          else: 
-             print('error message')
-          
-          print('2print')   
-          # protocol line 6:
-          
-          C_shares[mu+k,k] = shareh
+                  
+            broad_ckk= self.mult_shares(share_ran,C_shares[k,k])
+            self.broadcast('c_kk' + str(self.comr), broad_ckk)
+            
+            # protocol line 5 
+            r_temp.append(self.reconstruct_secret('c_kk'+str(self.comr))) 
+            print('r is:', r_temp)
+            print('1print')
+            if r_temp[k] == 0:
+               r.append(0)
+            elif r_temp[k] != 0:
+               r.append(1)
+            else: 
+               print('error message')
+            print('2print')   
+            # protocol line 6:
+            C_shares[mu+k,k] = shareh
 
-          f.append(shareh)
-   
-          sharet = self.mult_shares(sharet,shareh)    # mult shares med Beavers
+            f.append(shareh)
+     
+            sharet = self.mult_shares(sharet,shareh)    # mult shares med Beavers
 
-          
-          # protocol line 9
-          c_kk = (C_shares[k,k]+1-r[k])    # when c_kk !=0 then r will be 1 
-                                           # and 1-r will cancel out
+            
+            # protocol line 9
+            c_kk = (C_shares[k,k]+1-r[k])    # when c_kk !=0 then r will be 1 
+                                             # and 1-r will cancel out
+      
+            shareh = self.mult_shares(shareh,c_kk)
     
-          shareh = self.mult_shares(shareh,c_kk)
-          print('3PRINT')
-          for i in range(0, mu+k):
-              for j in range(k+1, nn+l):
-                  if i!= k and (i<= mu or j <= nn-1):
-                      # protocol line 13:
-                      dummy=np.matrix( np.vstack((C_shares[i,j],-(C_shares[i,k]))))
-                      temp= np.matrix(np.hstack(((C_shares[k,k]+1-r[k]), C_shares[k,j])))
-                      temp_C1=self.mult_shares(temp[0,0],dummy[0,0])
-                      temp_C2=self.mult_shares(temp[0,1],dummy[1,0])
-                      C_shares[i,j]=temp_C1 +temp_C2 #manuel computation 1x2 2x1 = 1x1
-                      print('if loop')          
-          print('updated C:', C_shares[0,0])
-        
+            for i in range(0, mu+k):
+                for j in range(k+1, nn+l):
+                    if i!= k and (i<= mu or j <= nn-1):
+                        # protocol line 13:
+                        dummy=np.matrix( np.vstack((C_shares[i,j],-(C_shares[i,k]))))
+                        temp= np.matrix(np.hstack(((C_shares[k,k]+1-r[k]), C_shares[k,j])))
+                        temp_C1=self.mult_shares(temp[0,0],dummy[0,0])
+                        temp_C2=self.mult_shares(temp[0,1],dummy[1,0])
+                        C_shares[i,j]=temp_C1 +temp_C2 #manuel computation 1x2 2x1 = 1x1
+                              
+            print('updated C:', C_shares[0,0])
+          
 
         
 p = party(F,int(x),n,t,pnr, q, q2, q3, party_addr, server_addr)
