5e1da76db0f59c5f9b0eb5c966b8e41625f2a861
andrealoevemaerke <andreatram@gmail.com>
2019-05-11 16:10:54 +0200

    ok

--- RP_impl/clouds.py
+++ RP_impl/clouds.py
@@ -306,7 +306,7 @@ class party(Thread):
         e11=np.array(np.zeros((nn,l)))
 
         C_shares=np.array(([e00[0,0], e00[0,1], e01[0,0]],[e00[1,0], e00[1,1], e01[1,0]], [e10[0,0], e10[0,1], 0],[ e10[1,0], e10[1,1], 0]))
-        C_shares.astype(int)
+
         #print('C matrix:', C_shares)
           
         f = []
