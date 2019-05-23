457f563bbc9b70494d0113febf0a76a26913892a
andrealoevemaerke <andreatram@gmail.com>
2019-05-23 16:41:25 +0200

    ok

--- RP_impl/clouds_party.py
+++ RP_impl/clouds_party.py
@@ -321,7 +321,7 @@ class party(Thread):
         
         print('x reshape ok')
         
-        self.broadcast('xxx' + str(self.comr), X[0])
+        self.broadcast('xxx' + str(self.comr), X[[0]])
     
         x_res= self.reconstruct_secret('xxx'+str(self.comr))
         
