f7cb2adcd06e619394556299e304d85739653f71
andrealoevemaerke <andreatram@gmail.com>
2019-05-23 16:25:22 +0200

    ok

--- RP_impl/clouds_party.py
+++ RP_impl/clouds_party.py
@@ -292,15 +292,15 @@ class party(Thread):
         gtL=gt_temp * L
         print('OK 2')
         
-        fx=np.zeros(2, dtype=int)  #np.matrix(np.zeros((2,1)))
+        fx=np.zeros(2)  #np.matrix(np.zeros((2,1)))
         ko1 = self.mult_shares(f[0], X[0]).n
         print('KO 1=', ko1)
         ko2 = self.mult_shares(f[1], X[1]).n
         print('KO 2=', ko2)
-        #for k in range(mu):
-            #fx[k] = self.mult_shares(f[k], X[k]).n
+        for k in range(mu):
+            fx[k] = self.mult_shares(f[k], X[k]).n
 
-        #print('YAY')
+        print('YAY')
         
         #fx=fx.astype(int)
         
