c36bec3217ba70be7cdb59aa445331952335c567
andrealoevemaerke <andreatram@gmail.com>
2019-05-23 15:52:35 +0200

    ok

--- RP_impl/clouds_party.py
+++ RP_impl/clouds_party.py
@@ -296,7 +296,7 @@ class party(Thread):
             print('fx is ', f)
             print('X is', X)
            
-            ko=self.mult_shares(f[0], X[0]).n
+            ko=self.mult_shares(f[1], X[1]).n
             print('ok:', ko)
             #for k in range(mu):
             #    fx[k] = self.mult_shares(f[k], X[k]).n
