7de73dffde4cb7d34acdb115b5ac64e8b458a3c1
andrealoevemaerke <50317013+andrealoevemaerke@users.noreply.github.com>
2019-05-24 09:05:43 +0200

    Add files via upload

---
+++ RP_impl/time_GE.m
@@ -0,0 +1,10 @@
+
+profile on 
+mat_GaussianElimination
+profileStruct = profile('info')
+
+
+[flopTotal, Details]=FLOPS('mat_GaussianElimination','MATfileName1',profileStruct);
+%time=timeit(ko)
+
+%FLOPS('GaussianElimination')
